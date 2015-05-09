e4b097ab2c4d4b9fd17bc2a2ca6df3458342bdb9
Mauricio Diaz <mauriciodiaz.1989@yahoo.com>
2015-05-09 00:34:15 -0700

    Revert "Revert "First Commit""
    This reverts commit 7bc74cc42af2e7da63dc2d453f37df8401f52635.

---
+++ Plexus/Assets/Scripts/A1Selection.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+
+public class A1Selection : MonoBehaviour {
+	
+
+
+	
+	public void South()
+	{
+		Application.LoadLevel ("A2");
+	}
+
+
+	//	public void ButtonPressed()
+	//	{
+	//		if (gameObject.name == "North")
+	//		{
+	//			Debug.Log("NORTH ");
+	//
+	//		}
+	//		else if (gameObject.name == "East")
+	//		{
+	//			Debug.Log("EAST ");
+	//
+	//
+	//		}
+	//		else if (gameObject.name == "South")
+	//		{
+	//			Debug.Log("SOUTH ");
+	//			correctAnswer = true;
+	//			
+	//		}
+	//		else if (gameObject.name == "West")
+	//		{
+	//			Debug.Log("WEST ");
+	//
+	//			
+	//		}
+	//	}
+
+
+}
